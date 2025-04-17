14040786bf5f4946e505c05ded76de948a9f2ad3
VAMSHI KRISHNA K <147719676+vamshik2506@users.noreply.github.com>
2025-04-17 16:33:25 +0530

    Update Calculator.java

--- src/main/java/com/example/Calculator.java
+++ src/main/java/com/example/Calculator.java
@@ -1,15 +1,7 @@
 package com.example;
 
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello, Jenkins!");
-    }
-
+public class Calculator {
     public int add(int a, int b) {
         return a + b;
-    }
-
-    public boolean isPositive(int number) {
-        return number > 0;
     }
 }
