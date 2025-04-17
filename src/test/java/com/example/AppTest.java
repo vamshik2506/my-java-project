30da058008189f7ae71fb537b8760e7de5d5de1b
VAMSHI KRISHNA K <147719676+vamshik2506@users.noreply.github.com>
2025-04-17 16:23:33 +0530

    Update AppTest.java

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,11 +1,29 @@
 package com.example;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class AppTest {
+
+    App app = new App();
+
     @Test
-    public void testApp() {
-        assertTrue(true);
+    public void testAdd() {
+        assertEquals(5, app.add(2, 3));
+    }
+
+    @Test
+    public void testAddNegative() {
+        assertEquals(-1, app.add(2, -3));
+    }
+
+    @Test
+    public void testIsPositiveTrue() {
+        assertTrue(app.isPositive(5));
+    }
+
+    @Test
+    public void testIsPositiveFalse() {
+        assertFalse(app.isPositive(-1));
     }
 }
