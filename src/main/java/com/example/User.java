6f4c64b8b0352753aeffcdbfd07ad6dfaf65b02c
VAMSHI KRISHNA K <147719676+vamshik2506@users.noreply.github.com>
2025-04-21 16:21:54 +0530

    Update User.java

--- src/main/java/com/example/User.java
+++ src/main/java/com/example/User.java
@@ -1,39 +1,19 @@
 package com.example;
 
 public class User {
-    private int id;
     private String name;
     private String email;
 
-    // Constructor
-    public User(int id, String name, String email) {
-        this.id = id;
+    public User(String name, String email) {
         this.name = name;
         this.email = email;
-    }
-
-    // Getters and Setters
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getEmail() {
         return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 }
